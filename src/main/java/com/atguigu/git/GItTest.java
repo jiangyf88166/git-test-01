a61379d44a9c46af39448e0436853f0dd5cc08a7
jyf2020 <jyf88166@163.com>
2022-04-30 20:46:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author jyf
+ * @create 2022-04-30-8:40 下午
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
