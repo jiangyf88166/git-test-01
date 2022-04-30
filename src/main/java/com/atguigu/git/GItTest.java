b99b8cfbc45eb6e680ea2ce875032008bc788ec8
jyf2020 <jyf88166@163.com>
2022-04-30 22:53:10 +0800

    second commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
